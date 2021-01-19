ec6627e151a430b9f1464c563074ea5c96ed6466
TakinosaJi <kostiantyn.chomakov@gmail.com>
2021-01-19 12:24:09 +0200

    Add new cmdlets to AzureDevOps module

--- src/Modules/AzureDevOps/AzureDevOps.psd1
+++ src/Modules/AzureDevOps/AzureDevOps.psd1
@@ -69,7 +69,7 @@
     # NestedModules = @()
      
     # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
-    FunctionsToExport = @('Get-Builds', 'Update-Build', 'Tag-Build')
+    FunctionsToExport = @('Get-Builds', 'Update-Build', 'Tag-Build', 'Create-PullRequest', 'Get-Repositories')
      
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
     CmdletsToExport = @()
--- src/Modules/AzureDevOps/AzureDevOps.psm1
+++ src/Modules/AzureDevOps/AzureDevOps.psm1
@@ -178,6 +178,124 @@ function Tag-Build {
 }
 
 
+
+function Create-PullRequest {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$RestApiUrl,    
+        [Parameter(Mandatory=$true)]
+        [string]$Collection,     
+        [Parameter(Mandatory=$true)]
+        [string]$Project,    
+        [Parameter(Mandatory=$true)]
+        [string]$Token,     
+        [Parameter(Mandatory=$true)]
+        [string]$RepositoryId,  
+        [Parameter(Mandatory=$true)]
+        [string]$SourceBranch, 
+        [Parameter(Mandatory=$true)]
+        [string]$TargetBranch, 
+        [Parameter(Mandatory=$false)]
+        [string]$AutoCompleteSetBy,
+        [Parameter(Mandatory=$false)]
+        [string]$ApiVersion = "5.0",
+        [Parameter(Mandatory=$false)]
+        [string]$Proxy,
+        [Parameter(Mandatory=$false)]
+        [int]$TimeoutSec = 300
+    )
+    
+    begin {
+        if (!$PSBoundParameters.ContainsKey('ErrorAction')) { $ErrorActionPreference = 'Stop' }
+
+        $commonParams = @{
+            ErrorAction = $ErrorActionPreference;
+            Verbose = $VerbosePreference -ne 'SilentlyContinue'
+        }
+
+        $moduleName = "WK.Utilities"
+        "$PSScriptRoot/../../Modules/$moduleName/$moduleName.psd1" | 
+            Import-Module -Force -Scope Global @commonParams 
+    }
+
+    process {
+        $authHeader = Get-BasicAuthHeader -Password $Token
+
+        $params = @{
+            Uri = "$RestApiUrl/$Collection/$Project/_apis/git/repositories/$RepositoryId/pullrequests?api-version=$ApiVersion"
+            Method = "POST"
+            Headers = @{ Authorization = $authHeader }
+            TimeoutSec =  $TimeoutSec
+        }   
+        if (!!$Proxy) {
+            $params.Proxy = $Proxy
+        }
+
+        $body = {
+            sourceRefName = $SourceBranch
+            targetRefName = $TargetBranch
+            title = "A new feature"
+            description = "Adding a new feature"
+        }
+        
+        return Invoke-RestMethod -ContentType 'application/json' `
+                                 -Body (ConvertTo-Json $body) `
+                                 @params `
+                                 @commonParams
+    }
+    end {}
+}
+
+
+
+function Get-Repositories {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$RestApiUrl,    
+        [Parameter(Mandatory=$true)]
+        [string]$Collection,     
+        [Parameter(Mandatory=$true)]
+        [string]$Project,    
+        [Parameter(Mandatory=$true)]
+        [string]$Token,
+        [Parameter(Mandatory=$false)]
+        [string]$Proxy,
+        [Parameter(Mandatory=$false)]
+        [int]$TimeoutSec = 300
+    )
+    
+    begin {
+        if (!$PSBoundParameters.ContainsKey('ErrorAction')) { $ErrorActionPreference = 'Stop' }
+
+        $commonParams = @{
+            ErrorAction = $ErrorActionPreference;
+            Verbose = $VerbosePreference -ne 'SilentlyContinue'
+        }
+
+        $moduleName = "WK.Utilities"
+        "$PSScriptRoot/../../Modules/$moduleName/$moduleName.psd1" | 
+            Import-Module -Force -Scope Global @commonParams 
+    }
+
+    process {
+        $authHeader = Get-BasicAuthHeader -Password $Token
+   
+        $params = @{
+            Uri = "$RestApiUrl/$Collection/$Project/_apis/git/repositories?api-version=$ApiVersion"
+            Method = "GET"
+            Headers = @{ Authorization = $authHeader }
+            TimeoutSec =  $TimeoutSec
+        }
+        if (!!$Proxy) {
+            $params.Proxy = $Proxy
+        }
+        
+        return Invoke-RestMethod @params @commonParams
+    }
+    end {}
+}
  
 #---------------------------------------
 #----EXPORTS----------------------------
